7187cc6a5ff2236996cb6f5bf1c77572ea67d26b
Justin Powers <jpowers4877@floridapoly.edu>
2020-01-26 12:30:44 -0500

    -Add Save Function -Start Loop Function

--- CurveTool-Powers4877/Source.cpp
+++ CurveTool-Powers4877/Source.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 using namespace cv;
@@ -8,6 +9,7 @@ using namespace cv;
 int main(int argc, char* argv[])
 {
 	VideoCapture cap("../barriers.avi");
+	VideoCapture oCap("../barriers.avi");
 
 	if (!cap.isOpened())
 	{
@@ -18,19 +20,31 @@ int main(int argc, char* argv[])
 	namedWindow("Test", WINDOW_NORMAL);
 	resizeWindow("Test", 500, 500);
 
-	double dFrames = cap.get(CAP_PROP_POS_FRAMES);
-
 	int sliderBrightness = -1;
 	int sliderContrast = -1;
 
+	int iBrightness = 0;
+	double dContrast = 0;
+
+	int frame_counter = 0;
+
+	int frames = cap.get(CAP_PROP_FRAME_COUNT);
+
+	Mat oFrame;
+	Mat nFrame;
+
 	while (true)
 	{
-		createTrackbar("Brightness", "Test", &sliderBrightness, 100);
+		//frame_counter++;
 
+		//if (frame_counter == frames) {
+		//	cap.set(CAP_PROP_POS_FRAMES, 0);
+		//}
+		createTrackbar("Brightness", "Test", &sliderBrightness, 100);
 		createTrackbar("Contrast", "Test", &sliderContrast, 100);
 
-		int iBrightness = sliderBrightness - 50;
-		double dContrast = sliderContrast / 50.0;
+		iBrightness = sliderBrightness - 50;
+		dContrast = sliderContrast / 50.0;
 
 		if (sliderBrightness == -1)
 		{
@@ -40,9 +54,6 @@ int main(int argc, char* argv[])
 		{
 			sliderContrast = 50;
 		}
-
-		Mat oFrame;
-		Mat nFrame;
 
 		cap.read(oFrame);
 		cap.read(nFrame);
@@ -65,7 +76,39 @@ int main(int argc, char* argv[])
 		}
 	}
 
+	int frame_width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
+	int frame_height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
+
+	Size frame_size(frame_width, frame_height);
+	int fps = 50;
+
+	VideoWriter oVideoWriter("../MyVideo.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, frame_size, true);
+
+	Mat Test;
+
+	for (;;)
+	{
+		bool is = oCap.grab();
+
+		if (is == false)
+		{
+			cout << "Cannot grab video frame" << endl;
+			break;
+		}
+		else
+		{
+			oCap.retrieve(Test);
+			Test.convertTo(Test, -1, dContrast, iBrightness);
+
+			cout << "Saving video" << endl;
+			oVideoWriter.write(Test); 
+			waitKey(10);
+		}
+	}
+
 	// When everything done, release the video capture object
+	oVideoWriter.release();
+	oCap.release();
 	cap.release();
 
 	// Closes all the frames
